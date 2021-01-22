6d5646352d764e9cc61ea628846f339f1eb8febc
Ethan Jinks O'Sullivan <ethanosullivan@users.noreply.github.com>
2021-01-22 14:14:52 -0500

    Merged date and feather functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 $(function () {
     // init feather icons
     feather.replace();
-});
 
-(function ($) {
     // Display Current Year 
     document.getElementById('current-year').innerHTML = dayjs().format('YYYY');
-})(jQuery);
+});
